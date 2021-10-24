d2d1eb386d869d4c5af625bdad85eba84b32ecd2
HojinAn <asdv94@naver.com>
2021-10-24 23:54:48 +0900

    using try exception finally

--- Java/ExceptionApp/CheckedExceptionApp.java
+++ Java/ExceptionApp/CheckedExceptionApp.java
@@ -3,13 +3,22 @@ import java.io.IOException;
 
 public class CheckedExceptionApp {
 	public static void main(String[] args) {
+		FileWriter f=null;
 		try {
-			FileWriter f = new FileWriter("data.txt");
+			f = new FileWriter("data.txt");
 			f.write("Creating txt file");
-			f.close();//우리가 실행하는 동안 이 txt 파일을 잡고 있던 걸 놔주는 작업
-			//checkedException은 실행 전에 프로그램이 알아서 exception 발생을 알려준다. 그래서 try catch로 처리해주기
-		}catch(IOException e) {
+			//close 발생 전에 Exception이 발생한다면 close가 실행되지 않아 난감한 일이 생길 수 있다.
+			//f.close();
+		}catch(IOException e) {//따라서 이렇게 Exception이 발생해도
 			e.printStackTrace();
+		} finally {//이 finally로 실행해줘서 닫히도록 한다.
+			if (f != null) {
+				try {
+					f.close();//IOException은 파일이 없으면 발생할 수 있으니까 얘도 예외처리 해줘야 한다.
+				} catch (IOException e) {
+					e.printStackTrace();//사실 이렇게 써놓으면 난잡하다..
+				}
+			}			
 		}
 
 	}
