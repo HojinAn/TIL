51a306a8fefb0a3c96591815d3aa7dbd01a0e314
HojinAn <asdv94@naver.com>
2022-01-10 01:09:05 +0900

    :recycle: Refactor ver. of study code

--- Online Judge/programmers/숫자 문자열과 영단어/solution.js
+++ Online Judge/programmers/숫자 문자열과 영단어/solution.js
@@ -1,17 +1,29 @@
-function solution(s) {
-  const list_num = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-  ];
+const nums = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+const numMap = new Map([
+  ["zero", 0],
+  ["one", 1],
+  ["two", 2],
+  ["three", 3],
+  ["four", 4],
+  ["five", 5],
+  ["six", 6],
+  ["seven", 7],
+  ["eight", 8],
+  ["nine", 9],
+]);
 
+const solution = (s) => {
   let result = "";
   let temp = "";
   for (let char of s) {
@@ -19,13 +31,13 @@ function solution(s) {
       result += char;
       continue;
     }
+
     temp += char;
-    if (list_num.indexOf(temp) > -1) {
-      result += list_num.indexOf(temp);
+    if (numMap.get(temp)) {
+      result += numMap.get(temp);
       temp = "";
     }
   }
-  return +result;
-}
 
-console.log(solution("onetwo1threezerofourninefive6seven"));
+  return +result;
+};
