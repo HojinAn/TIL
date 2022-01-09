af36d01966d4f0bac368b2350d20a78e079f7a44
HojinAn <asdv94@naver.com>
2022-01-10 01:15:07 +0900

    :recycle: Much better than mine same idea
    광훈이형 코드, 아이디어는 비슷했으나 훨씬 보기 좋고 정리된 코드

--- Online Judge/programmers/숫자 문자열과 영단어/solution.js
+++ Online Judge/programmers/숫자 문자열과 영단어/solution.js
@@ -1,43 +1,34 @@
-const nums = [
-  "zero",
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine",
-];
-const numMap = new Map([
-  ["zero", 0],
-  ["one", 1],
-  ["two", 2],
-  ["three", 3],
-  ["four", 4],
-  ["five", 5],
-  ["six", 6],
-  ["seven", 7],
-  ["eight", 8],
-  ["nine", 9],
-]);
-
-const solution = (s) => {
+function solution(s) {
   let result = "";
-  let temp = "";
-  for (let char of s) {
-    if (!isNaN(+char)) {
+  let i = 0;
+
+  const numLengths = [4, 3, 3, 5, 4, 4, 3, 5, 5, 4];
+
+  while (i < s.length) {
+    let num;
+
+    const char = s[i];
+
+    if (char === "z") num = 0;
+    else if (char === "o") num = 1;
+    else if (char === "t" && s[i + 1] === "w") num = 2;
+    else if (char === "t" && s[i + 1] === "h") num = 3;
+    else if (char === "f" && s[i + 1] === "o") num = 4;
+    else if (char === "f" && s[i + 1] === "i") num = 5;
+    else if (char === "s" && s[i + 1] === "i") num = 6;
+    else if (char === "s" && s[i + 1] === "e") num = 7;
+    else if (char === "e") num = 8;
+    else if (char === "n") num = 9;
+    else {
       result += char;
+      i += 1;
+
       continue;
     }
 
-    temp += char;
-    if (numMap.get(temp)) {
-      result += numMap.get(temp);
-      temp = "";
-    }
+    result += `${num}`;
+    i += numLengths[num];
   }
 
   return +result;
-};
+}
