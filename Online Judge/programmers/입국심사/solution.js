03c49733b039332adb5818ab8921640bea036577
HojinAn <asdv94@naver.com>
2022-02-05 01:17:28 +0900

    programmers 입국 심사 js solution

---
+++ Online Judge/programmers/입국심사/solution.js
@@ -0,0 +1,22 @@
+// https://programmers.co.kr/learn/courses/30/lessons/43238
+function solution(n, times) {
+  times.sort((a, b) => a - b);
+  let min_time = 1,
+    max_time = n * times[times.length - 1],
+    mid,
+    peopleSumEachCase;
+  while (min_time <= max_time) {
+    mid = Math.floor((max_time + min_time) / 2);
+    peopleSumEachCase = 0;
+    for (let time of times) {
+      peopleSumEachCase += Math.floor(mid / time);
+    }
+    if (peopleSumEachCase >= n) {
+      max_time = mid - 1;
+    } else {
+      min_time = mid + 1;
+    }
+  }
+
+  return max_time + 1;
+}
